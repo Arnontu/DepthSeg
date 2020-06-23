7703d26075d9574fe42077df52387be1321ef923
Arnontu <arnontu@gmail.com>
2020-06-23 13:39:22 +0300

    Update train-pqr.sh

--- train-pqr.sh
+++ train-pqr.sh
@@ -1,4 +1,5 @@
 cd ..
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 # Set up the working environment.
 CURRENT_DIR=$(pwd)
 WORK_DIR="${CURRENT_DIR}/deeplab"
@@ -24,8 +25,7 @@ python "${WORK_DIR}"/train.py \
   --atrous_rates=18 \
   --output_stride=16 \
   --decoder_output_stride=4 \
-  --train_crop_size=513 \
-  --train_crop_size=513 \
+  --train_crop_size=513,513 \
   --train_batch_size=4 \
   --training_number_of_steps="${NUM_ITERATIONS}" \
   --fine_tune_batch_norm=true \
