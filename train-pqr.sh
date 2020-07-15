5010b049211072c713f8bd95389c20ca2836377c
Arnontu <arnontu@gmail.com>
2020-07-15 10:14:31 +0300

    Update train-pqr.sh

--- train-pqr.sh
+++ train-pqr.sh
@@ -15,7 +15,7 @@ DATASET="${WORK_DIR}/${DATASET_DIR}/${PQR_FOLDER}/tfrecord"
 mkdir -p "${WORK_DIR}/${DATASET_DIR}/${PQR_FOLDER}/exp"
 mkdir -p "${TRAIN_LOGDIR}"
 
-NUM_ITERATIONS=20000
+NUM_ITERATIONS=3000
 python "${WORK_DIR}"/train.py \
   --logtostderr \
   --train_split="train" \
@@ -26,9 +26,12 @@ python "${WORK_DIR}"/train.py \
   --output_stride=16 \
   --decoder_output_stride=4 \
   --train_crop_size=513,513 \
-  --train_batch_size=4 \
+  --train_batch_size=2 \
   --training_number_of_steps="${NUM_ITERATIONS}" \
-  --fine_tune_batch_norm=true \
+  --fine_tune_batch_norm=false \
+  --initialize_last_layer=false \
+  --last_layers_contain_logits_only=true \
   --tf_initial_checkpoint="${INIT_FOLDER}/deeplabv3_pascal_train_aug/model.ckpt" \
   --train_logdir="${TRAIN_LOGDIR}" \
+  --dataset='pqr' \
   --dataset_dir="${DATASET}"
